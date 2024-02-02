e3048c8e4c4ed4a3433882e0cced6f24a0860815
Suraj Perumallapalli <surajperumallapalli97@gmail.com>
2024-02-02 12:46:09 +0530

    feat: create cpp, py files with title input

--- title_to_file.py
+++ title_to_file.py
@@ -1,6 +1,19 @@
-title_name = input('Enter question title as is: ')
+import sys
 
-title_name = title_name.replace('. ', '_').replace(' ', '_')
+title_name = None
 
-print(title_name.lower())
+if len(sys.argv) == 1:
+	title_name = input('Enter question title as is: ')
+else:
+	title_name = sys.argv[1]
+
+title_name = title_name.replace('. ', '_').replace(' ', '_').lower()
+
+cpp_file = f"{title_name}.cpp"
+py_file = f"{title_name}.py"
+
+open(cpp_file, 'a').close()
+open(py_file, 'a').close()
+
+print(f"{cpp_file}, {py_file} created")
 
