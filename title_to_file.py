3d937a1c1b82c01684832aecc226796432c0fdba
Suraj Perumallapalli <surajperumallapalli97@gmail.com>
2024-02-17 23:50:09 +0530

    refactor: modify destination for testcase files

--- title_to_file.py
+++ title_to_file.py
@@ -9,8 +9,9 @@ else:
 
 title_name = title_name.replace('. ', '_').replace(' ', '_').lower()
 
+test_folder = "test_cases"
 cpp_file = f"{title_name}.cpp"
-py_file = f"{title_name}.py"
+py_file = f"{test_folder}/{title_name}.py"
 
 open(cpp_file, 'a').close()
 open(py_file, 'a').close()
