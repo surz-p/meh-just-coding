57469200d0b0b37aac3c74727e20f03d0c2c1db8
Suraj Perumallapalli <surajperumallapalli97@gmail.com>
2024-03-15 01:17:34 +0530

    feat: intro to volumes
    this includes intro to named volumes, anonymous volumes
    and bind-mounts

--- docker_kubernetes/section_3_managing_data/data-volumes-01-starting-setup/server.js
+++ docker_kubernetes/section_3_managing_data/data-volumes-01-starting-setup/server.js
@@ -23,6 +23,7 @@ app.get('/exists', (req, res) => {
 });
 
 app.post('/create', async (req, res) => {
+  console.log('debug nodemon XD')
   const title = req.body.title;
   const content = req.body.text;
 
@@ -36,7 +37,8 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.rename(tempFilePath, finalFilePath);
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
       res.redirect('/');
     }
   });
